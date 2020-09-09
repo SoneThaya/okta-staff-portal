fa970c9b0e653268c7b45dfb4b1135ddb7d6f5db
Sone <sthaya@gmail.com>
2020-09-08 19:52:07 -0700

    set up routing path for home and staff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Route path="/" exact component={Home} />
+          <Route path="/staff" exact component={Staff} />
+        </div>
+      </div>
+    </Router>
+    
   );
 }
 
