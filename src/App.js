75491d11b8be223430e24b559f4ed74c6b35bc6c
Sone <sthaya@gmail.com>
2020-09-08 20:26:29 -0700

    set up sign in widget and login component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 import './App.css';
 
+function onAuthRequired({ history }) {
+  history.push('/login')
+}
+
 function App() {
+
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route path="/" exact component={Home} />
-          <Route path="/staff" exact component={Staff} />
+      <Security
+        issuer={`${process.env.REACT_APP_OKTA_ISSUER_URI}/oauth2/default`}
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        redirectUri={window.location.origin + '/implicit/callback'}
+        onAuthRequired={onAuthRequired}
+        pkce={true} >
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact component={Home} />
+            <SecureRoute path="/staff" exact component={Staff} />
+            <Route
+              path='/login'
+              render={() => <Login issuer={`${process.env.REACT_APP_OKTA_ISSUER_URI}/oauth2/default`} />} />
+            <Route path='/implicit/callback' component={LoginCallback} />
+          </div>
         </div>
-      </div>
+      </Security>
+      
     </Router>
     
   );
